57ff5c6c8c08f80517833ffd93511b06d56e5eab
Adam Gencarelli <agencare@uwo.ca>
2014-12-01 17:00:33 -0500

    removed some unnecessary things

--- public/scripts/view_data_timespan.js
+++ public/scripts/view_data_timespan.js
@@ -35,56 +35,5 @@ $(document).ready(function() {
                 }
             }
         })
-
-        /*
-        $.ajax({ 
-            url: '/view_data_timespan/' + startDate + '/' + endDate,
-            type: "POST",
-            //dataType: "json",
-            async: false,
-            cache: false,
-            data: { 
-                monthly: false
-            },
-            success: function (data) { 
-                $.ajax({ 
-                    url: '/view_data_timespan', 
-                    type: "POST", 
-                    //dataType: "json", 
-                    cache: true, 
-                    data: { 
-                        monthly: false
-                    },
-                    success: function(data) {
-                        console.log(data);
-                        $("#readings").dataTable({
-                            data: data,
-                            columns: [
-                                { data: "day", sTitle: "Day", sClass: "center" },
-                                { data: "reading", sTitle: "Your Meter Reading", sClass: "center" },
-                                { data: "price", sTitle: "Price", sClass: "center"}
-                            ],
-                
-                            bSort : false,
-                            bFilter : false
-                        });
-                    },
-                    error: function (jqXHR, textStatus, errorThrown) {
-                        if (typeof errorFn !== 'undefined') {
-                            errorFn(jqXHR, textStatus, errorThrown);
-                        }
-                    }
-                });
-            }, 
-            error: function (jqXHR, textStatus, errorThrown) { 
-                console.log("it failed"); 
-                console.log(errorThrown);
-                console.log(textStatus); 
-                console.log(jqXHR);
-                if (typeof errorFn !== 'undefined') {
-                    errorFn(jqXHR, textStatus, errorThrown);
-                }
-            }
-        }); */
     });
 });
